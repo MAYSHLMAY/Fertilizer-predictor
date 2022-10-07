4fc48379e96829e47fc65a7fa756bdb7d81ce762
Mikiyas <educationforlife424@gmail.com>
2022-10-07 16:46:44 +0300

    Added to a repo

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,12 @@
+import Image from "next/image"
+
 export default function NavBar() {
     return (
         <>
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
           <div className="container">
             <a className="navbar-brand" href="./index.html">
-              <img src="../static/images/logo.jpg" className="logo" alt="logo" />
+              <Image src="../static/images/logo.jpg" className="logo" alt="logo" />
             </a>
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               <span className="fas fa-bars" />
