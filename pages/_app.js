1c358668c582f9feef8f9a9aaed4597919fa4d6c
Mikiyas <educationforlife424@gmail.com>
2022-10-07 16:39:29 +0300

    files addeds

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" /> <link rel="stylesheet" href="../static/all.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" /> <link rel="stylesheet" href="../static/all.css"/>
     
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
