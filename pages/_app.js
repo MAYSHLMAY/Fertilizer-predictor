951967304dc6ef9640c8081c9aa44e65ae644ea4
Mikiyas <educationforlife424@gmail.com>
2022-10-07 19:14:24 +0300

    Added

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ export default function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <Script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'>
       </Script>
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></Script>
 
     </>
   );
