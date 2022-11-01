0ef502b2d1607e459ffaab9f7eec4aff6d8b78b0
Mikiyas <educationforlife424@gmail.com>
2022-11-02 01:13:29 +0300

    Done

--- components/footer.js
+++ components/footer.js
@@ -5,14 +5,14 @@ export default function Footer() {
           <div className="container bottom_border">
             <div className="row">
               <div className="col-lg-4 col-md-6 col-sm-6 col">
-                <h5 className="headin5_amrc col_white_amrc pt2">Find us</h5>
+                <h5 className="headin5_amrc col_white_amrc pt2">Find Me</h5>
                 {/*headin5_amrc*/}
-                <p><i className="fa fa-location-arrow" />Kotebe Educational University</p>
+                <p><i className="fa fa-location-arrow" />Bethel, Addis Ababa</p>
                 <p><i className="fa fa-phone" /> +251-900766164 </p>
                 <p><i className="fa fa fa-envelope" />Amarasfaw1077@gmail.com </p>
               </div>
               <div className="col-lg-4 col-md-6 col-sm-6 col">
-                <h5 className="headin5_amrc col_white_amrc pt2">Follow us</h5>
+                <h5 className="headin5_amrc col_white_amrc pt2">Follow Me</h5>
                 {/*headin5_amrc ends here*/}
                 <ul className="footer_ul2_amrc">
                   <li>
@@ -21,7 +21,6 @@ export default function Footer() {
                     </p>
                   </li>
                   <li>
-                    <p><a href="#">Yisakor Eyob</a></p>
                   </li>
                 </ul>
                 {/*footer_ul2_amrc ends here*/}
@@ -32,7 +31,7 @@ export default function Footer() {
                 <ul className="footer_ul_amrc">
                   <li><a href="https://farmer.gov.in/stl.aspx">Soil Testing labs (State-wise)</a></li>
                   <li><a href="https://www.napanta.com/fertilizer-dealer">Offline fertilizer dealers (State-wise)</a></li>
-                  <li><a href="https://www.napanta.com/market-price/telangana/warangal/warangal">Daily Market Price (of various crops)</a></li>
+                  <li><a href="https://www.napanta.com/market-price/telangana/warangal/warangal">Daily Market Price (of variMe crops)</a></li>
                   <li><a href="https://www.ugaoo.com/plant-care/plant-growth/fertilizers.html">Buy fertilizers online</a></li>
                   <li><a href="https://agricoop.gov.in/programmes-schemes-listing">Programmes and Schemes</a></li>
                   <li><a href="https://pib.gov.in/PressReleasePage.aspx?PRID=1725612">MSP (crop-wise)</a></li>
